817b9623a8f1a661adcb8f9252ef268c6a9267ee
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-25 12:05:37 +0200

    create Home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/app_style.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+           Container(
+             padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Good Morning', style: Styles.headlineStyle3),
+                        const Gap(5),
+                        Text('Book Tickets', style: Styles.headlineStyle),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/images/img_1.png')
+                        )
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
