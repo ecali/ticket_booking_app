6404234db359d736f483a1034dbeb11b6e35f999
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-25 12:05:27 +0200

    Update Bottom Bar
    Remove appBar
    add HomeScreen

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,6 +1,7 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_booking_app/screens/home_screen.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
@@ -10,9 +11,9 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
   static final List<Widget>_widgetOptions = <Widget>[
-    const Text('Home'),
+    HomeScreen(),
     const Text('Search'),
     const Text('Tickets'),
     const Text('Profile')
@@ -26,9 +27,6 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My tickcets')
-      ),
       body: Center(
           child: _widgetOptions[_selectedIndex]
       ),
