44c0a71c95267f2651cd0b9909a5c3ab0622b467
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-29 10:25:05 +0200

    add Scroll for ticket

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -75,7 +75,17 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           const Gap(15),
-          TicketView(),
+          SingleChildScrollView(
+           scrollDirection: Axis.horizontal,
+           padding: const EdgeInsets.only(left: 20),
+           child: Row(
+             children: [
+               TicketView(),
+               TicketView(),
+             ],
+           ),
+          )
+
         ],
       ),
     );
