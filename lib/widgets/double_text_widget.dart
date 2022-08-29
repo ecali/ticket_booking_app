9f0ffbefc47e61fa785450921b6995906f7674d7
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-29 12:25:59 +0200

    Create double_text_widget.dart

---
+++ lib/widgets/double_text_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/app_style.dart';
+
+class AppDoubleTextWidget extends StatelessWidget {
+  final String bigText;
+  final String smallText;
+  const AppDoubleTextWidget({Key? key, required this.bigText, required this.smallText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(bigText, style: Styles.headlineStyle2),
+        InkWell(
+            onTap: (){
+              print('you are tapped');
+            },
+            child: Text(smallText, style: Styles.textStyle.copyWith(color: Styles.primaryColor))
+        )
+      ],
+    );
+  }
+}
