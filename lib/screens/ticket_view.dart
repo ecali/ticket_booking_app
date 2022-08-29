07195fc580b9f41efa66c2f8a19c42fb86a5faa0
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-29 10:24:46 +0200

    finish ticket

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -13,10 +13,10 @@ class TicketView extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = AppLayout.getSize(context);
     return SizedBox(
-      width: size.width,
+      width: size.width*0.85,
       height: 200,
       child: Container(
-        margin: const EdgeInsets.only(left: 16),
+        margin: const EdgeInsets.only(right: 16),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -33,7 +33,7 @@ class TicketView extends StatelessWidget {
                     children: [
                       Text('NYC', style: Styles.headlineStyle3.copyWith(color: Colors.white)),
                       const Spacer(),
-                      ThickContainer(),
+                      const ThickContainer(),
                       Expanded(child: Stack(
                         children : [
                         SizedBox(
@@ -44,8 +44,8 @@ class TicketView extends StatelessWidget {
                                 direction: Axis.horizontal,
                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 mainAxisSize: MainAxisSize.max,
-                                children: List.generate((constraints.constrainWidth() / 6).floor(), (index) => SizedBox(
-                                  width: 3, height: 1, child: DecoratedBox(decoration: BoxDecoration(
+                                children: List.generate((constraints.constrainWidth() / 6).floor(), (index) => const SizedBox(
+                                  width: 3, height: 1, child: const DecoratedBox(decoration: BoxDecoration(
                                     color: Colors.white
                                 ),),
                                 )),
@@ -54,10 +54,10 @@ class TicketView extends StatelessWidget {
 
                           ),
                         ),
-                        Center(child: Transform.rotate(angle: 1.5, child: Icon(Icons.local_airport_rounded, color: Colors.white))),
+                        Center(child: Transform.rotate(angle: 1.5, child: const Icon(Icons.local_airport_rounded, color: Colors.white))),
                         ]
                       )),
-                      ThickContainer(),
+                      const ThickContainer(),
                       const Spacer(),
                       Text('LDN', style: Styles.headlineStyle3.copyWith(color: Colors.white))
                     ],
@@ -80,7 +80,98 @@ class TicketView extends StatelessWidget {
             ),
             /* showing the orange part of the card/ticket */
             Container(
-
+              color: Styles.orangeColor,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10)
+                        )
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: LayoutBuilder(
+                      builder: (BuildContext context, BoxConstraints constraints) {
+                        return Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children: List.generate((constraints.constrainWidth()/15).floor(), (index) => const SizedBox(
+                            width: 5, height: 1,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                  color: Colors.white
+                              ),
+                            ),
+                          )),
+                        );
+                      },
+                    ),
+                  )),
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            /* Bottom part of the orange card/ticket */
+            Container(
+              decoration: BoxDecoration(
+                  color: Styles.orangeColor,
+                  borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(21), bottomRight: Radius.circular(21))
+              ),
+              padding: const EdgeInsets.only(left: 16, top: 10, right: 16, bottom: 16),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('1 MAY', style: Styles.headlineStyle3.copyWith(color: Colors.white)),
+                          const Gap(5),
+                          Text('Date', style: Styles.headlineStyle4.copyWith(color: Colors.white)),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('08:00 AM', style: Styles.headlineStyle3.copyWith(color: Colors.white)),
+                          const Gap(5),
+                          Text('Departure time', style: Styles.headlineStyle4.copyWith(color: Colors.white)),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text('23', style: Styles.headlineStyle3.copyWith(color: Colors.white)),
+                          const Gap(5),
+                          Text('Number', style: Styles.headlineStyle4.copyWith(color: Colors.white)),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
