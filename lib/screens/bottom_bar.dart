1d80df6c4d321ff412489df931c563dfdaa191d5
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-29 12:26:07 +0200

    Update bottom_bar.dart

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -2,6 +2,7 @@ import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_booking_app/screens/home_screen.dart';
+import 'package:ticket_booking_app/screens/search_screen.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
@@ -13,8 +14,8 @@ class BottomBar extends StatefulWidget {
 class _BottomBarState extends State<BottomBar> {
   int _selectedIndex = 0;
   static final List<Widget>_widgetOptions = <Widget>[
-    HomeScreen(),
-    const Text('Search'),
+    const HomeScreen(),
+    const SearchScreen(),
     const Text('Tickets'),
     const Text('Profile')
   ];
