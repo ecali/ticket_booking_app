96266ae5211cd3ceadbb152761ae5255fd4c2665
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-29 10:24:50 +0200

    Update app_style.dart

--- lib/utils/app_style.dart
+++ lib/utils/app_style.dart
@@ -5,7 +5,7 @@ class Styles {
   static Color prymaryColor = primary;
   static Color textColor = const Color(0xFF3b3b3b);
   static Color bgColor = const Color(0xFFeeedf2);
-  static Color orangeColor = const Color(0xFF526799);
+  static Color orangeColor = const Color(0xFFF37B67);
   static TextStyle textStyle = TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
   static TextStyle headlineStyle = TextStyle(fontSize: 26, color: textColor, fontWeight: FontWeight.bold);
   static TextStyle headlineStyle2 = TextStyle(fontSize: 21, color: textColor, fontWeight: FontWeight.bold);
