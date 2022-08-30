aecae67ee183b5c1bcced994aa83d933d21ad3d6
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-30 14:46:51 +0200

    Update ticket_screen.dart

--- lib/screens/ticket_screen.dart
+++ lib/screens/ticket_screen.dart
@@ -7,7 +7,7 @@ import 'package:ticket_booking_app/utils/app_layout.dart';
 import 'package:ticket_booking_app/utils/app_style.dart';
 import 'package:ticket_booking_app/widgets/column_layout.dart';
 import 'package:ticket_booking_app/widgets/ticket_tabs.dart';
-
+import 'package:barcode_widget/barcode_widget.dart';
 import '../widgets/layout_builder_widget.dart';
 
 class TicketScreen extends StatelessWidget {
@@ -58,9 +58,61 @@ class TicketScreen extends StatelessWidget {
                     ),
                     Gap(AppLayout.getHeight(20)),
                     const AppLayoutBuilderWidget(sections: 15, isColor: false, width: 5),
+                    Gap(AppLayout.getHeight(20)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset('assets/images/visa.png', scale: 11),
+                                Text(' ***2462', style: Styles.headlineStyle3)
+                              ],
+                            ),
+                            Gap(5),
+                            Text('Payment method', style: Styles.headlineStyle4,)
+                          ],
+                        ),
+                        const AppColumnLayout(firstText: '\$249.99', secondText: 'Price', alignment: CrossAxisAlignment.end, isColor: false)
+                      ],
+                    ),
                   ],
                 ),
-              )
+              ),
+              /*
+              * BARCODE
+              */
+              const SizedBox( height: 1 ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(AppLayout.getHeight(21)),
+                      bottomLeft: Radius.circular(AppLayout.getHeight(21)))
+                ),
+                margin: EdgeInsets.only(left: AppLayout.getHeight(15), right: AppLayout.getHeight(15)),
+                padding: EdgeInsets.only(top: AppLayout.getHeight(20), bottom: AppLayout.getHeight(20)),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(AppLayout.getHeight(15)),
+                    child: BarcodeWidget(
+                      barcode: Barcode.code128(),
+                      data: 'https://github.com/ecali',
+                      drawText: false,
+                      color: Styles.textColor,
+                      width: double.infinity,
+                      height: 70,
+                    ),
+                  ),
+                ),
+              ),
+              Gap(AppLayout.getHeight(20)),
+              Container(
+                padding: EdgeInsets.only(left: AppLayout.getHeight(15)),
+                child: TicketView(ticket: ticketList[0]),
+              ),
             ],
           )
         ],
