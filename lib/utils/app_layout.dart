7abf1aed5b4cda56d71edd5f4686a0cb2e2301ab
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-31 12:34:25 +0200

    centralized the Gap and gap size

--- lib/utils/app_layout.dart
+++ lib/utils/app_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:ticket_booking_app/utils/app_utils.dart';
 
 class AppLayout{
   static getSize(BuildContext context){
@@ -18,5 +20,31 @@ class AppLayout{
   static getWidth(double pixels){
     double x = getScreenWidth()/pixels;
     return getScreenWidth()/x;
+  }
+  static addGap(GapSize size){
+    switch(size){
+      case GapSize.mini :
+        return Gap(getHeight(2));
+      case GapSize.doubleMini :
+        return Gap(getHeight(4));
+      case GapSize.small :
+        return Gap(getHeight(5));
+      case GapSize.moreSmall :
+        return Gap(getHeight(8));
+      case GapSize.doubleSmall :
+        return Gap(getHeight(10));
+      case GapSize.lessMedium :
+        return Gap(getHeight(12));
+      case GapSize.medium :
+        return Gap(getHeight(15));
+      case GapSize.moreMedium :
+        return Gap(getHeight(20));
+      case GapSize.big :
+        return Gap(getHeight(25));
+      case GapSize.veryBig :
+        return Gap(getHeight(40));
+      default:
+        return Gap(getHeight(1));
+    }
   }
 }
--- lib/utils/app_utils.dart
+++ lib/utils/app_utils.dart
@@ -6,5 +6,19 @@ class AppUtils {
     final DateFormat formatter = DateFormat('dd MMM yyyy');
     return formatter.format(now);
   }
+
+}
+enum GapSize {
+  mini, // 2
+  doubleMini, // 4
+  small, // 5
+  moreSmall, // 8
+  doubleSmall, // 10
+  lessMedium, // 12
+  medium, // 15
+  moreMedium, // 20
+  big, // 25
+  veryBig, // 40
+  one, // 1
 }
 
