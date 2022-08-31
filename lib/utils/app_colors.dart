e922a826144f5170677c47908fd03553a3ddc1d9
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-08-31 11:19:48 +0200

    Create app_colors.dart

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
+  static Color orangeColor = const Color(0xFFF37B67);
+  static Color kakiColor = const Color(0xFFd2bdb6);
+}
